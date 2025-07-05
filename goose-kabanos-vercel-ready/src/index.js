f945b0eda13fbb0834bd3011c0880cc74c254fe8
Fullmetal1080 <Nirbrill@gmail.com>
2025-07-05 20:26:30 +0300

    Delete goose-kabanos-vercel-ready directory

--- goose-kabanos-vercel-ready/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Game from './Game';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Game />);
